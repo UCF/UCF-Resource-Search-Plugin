385d35ef6fa89a5db8d7c8cbb9450be7559b6da2
Jo Dickson <jogreybill@gmail.com>
2021-05-14 12:07:49 -0400

    Updated version number and changelog

--- ucf-resource-search.php
+++ ucf-resource-search.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Resource Search
 Description: Provides a custom post type, shortcode, functions, and default styles for displaying a resource search input and list of resources.
-Version: 1.0.6
+Version: 1.0.7
 Author: UCF Web Communications
 License: GPL3
 */
